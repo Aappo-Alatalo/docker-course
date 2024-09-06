f982c8c9549c855403ecbb293338ea777a66092b
Aappo Alatalo <aappo.alatalo@gmail.com>
2024-09-06 14:13:55 +0300

    Add exercise 3.4 and fix exercise 3.3 mishap

--- part3/3.4/builder.sh
+++ part3/3.4/builder.sh
@@ -3,9 +3,13 @@
 GITHUB_REPO=$1
 DOCKER_HUB_REPO=$2
 
-# Clone the GitHub repository
+# Log in to Docker Hub
+echo "$DOCKER_PWD" | docker login --username "$DOCKER_USER" --password-stdin
+
+# Clone the GitHub repository, had to change to http since container doesn't have my ssh key
 echo "Cloning GitHub repository: $GITHUB_REPO ..."
-git clone "git@github.com:$GITHUB_REPO.git"
+git clone "https://github.com/$GITHUB_REPO.git"
+
 
 # Navigate to the cloned repository directory
 cd $(basename "$GITHUB_REPO")
